488c41eec4072080aef84e2deeeeba0469f0236c
BugHunter-X <askzenai@gmail.com>
2024-12-29 08:12:04 +0100

    Add bug.sol file

---
+++ bug.sol
@@ -0,0 +1,9 @@
+function getBalance() {
+  const balance = await web3.eth.getBalance(address);
+  return balance;
+}
+
+// ... later in the code ...
+
+const balance = getBalance(); // Missing await keyword
+console.log(balance);
